3db67084a98f9b42a84f272d9c122be58c037775
jaison7slayer <roejusa7@gmail.com>
2023-09-14 00:10:43 -0500

    Se sube a la rama develop la base de la api

---
+++ src/main/java/com/technical/challenge2/app/payment/technicalchallengepayment/TechnicalChallengePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.technical.challenge2.app.payment.technicalchallengepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalChallengePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalChallengePaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technical/challenge2/app/payment/technicalchallengepayment/TechnicalChallengePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technical.challenge2.app.payment.technicalchallengepayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalChallengePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
